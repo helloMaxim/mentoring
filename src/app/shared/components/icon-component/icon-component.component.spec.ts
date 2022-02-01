5de50918aae10eadbf1cac4d3c84516061fafac1
mdurnenko <mdurnenko@langate.com>
2022-02-01 16:56:14 +0200

    new project

---
+++ src/app/shared/components/icon-component/icon-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IconComponentComponent } from './icon-component.component';
+
+describe('IconComponentComponent', () => {
+  let component: IconComponentComponent;
+  let fixture: ComponentFixture<IconComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ IconComponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IconComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
